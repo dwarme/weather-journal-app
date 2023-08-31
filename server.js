cc66812570bd09dee61ef2f85e1dfa01ed9c508e
Daouda Warme <dwarme99@gmail.com>
2023-08-31 13:50:51 +0200

    comment

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const UUI = require('uuid').v4
+
+const PORT = 4002;
+
 // Setup empty JS object to act as endpoint for all routes
 const projectData = {};
 
-const PORT = 4002;
 // Require Express to run server and routes
 const express = require('express')
 
 // Start up an instance of app
 const app = express();
+
 /* Middleware*/
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -21,12 +25,15 @@ app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Initialize all route with a callback function
+
 app.get('/all', (_, res) => {
     res.json(projectData)
 })
 
 app.post('/add', (req, res)=>{
     const {date, temp, feelings} = req.body;
+
     const KEY = UUI()
     projectData[KEY] = {date, temp, feelings};
 
